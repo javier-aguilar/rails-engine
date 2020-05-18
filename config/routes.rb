9edfdf15c4b0e462ca82d9e198b99d6115655d6d
Javier Aguilar <jvr.agx@gmail.com>
2020-05-18 17:51:27 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      scope module: "merchants" do
+        get 'merchants/find_all', to: 'search#index'
+        get 'merchants/find', to: 'search#show'
+        get 'merchants/most_revenue', to: 'revenue#index'
+        get 'merchants/most_items', to: 'most_items#index'
+      end
+
+      scope module: "items" do
+        get 'items/find_all', to: 'search#index'
+        get 'items/find', to: 'search#show'
+      end
+
+      resources :merchants do
+        scope module: "merchants" do
+          resources :items
+        end
+      end
+
+      resources :items do
+        scope module: "items" do
+          resources :merchant, only: [:index]
+        end
+      end
+    end
+  end
 end
