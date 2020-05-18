3a9dd08b947dbf1a2729c91d1c43b8517fe4b710
Javier Aguilar <jvr.agx@gmail.com>
2020-05-18 17:52:00 -0600

    Update model relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,6 @@ class Invoice < ApplicationRecord
 
   belongs_to :merchant
   belongs_to :customer
+  has_many :invoice_items
   has_many :transactions, dependent: :destroy
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
 
-  has_many :invoice_items, dependent: :destroy
   has_many :items, dependent: :destroy
+  has_many :invoices
 end
