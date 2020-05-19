aafda428f7dab9b74f5808ed19568101dd7d4719
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 16:45:35 -0600

    Clean-up item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,11 @@ class Item < ApplicationRecord
 
   belongs_to :merchant
 
-  scope :filter_by_name, -> (name) { where('name ILIKE ?', "%#{name}%") }
-  scope :filter_by_description, -> (description) { where('description ILIKE ?', "%#{description}%") }
-  scope :filter_by_unit_price, -> (unit_price) { where unit_price: unit_price }
-  scope :filter_by_updated_at, -> (updated_at) { where updated_at: updated_at }
-  scope :filter_by_created_at, -> (created_at) { where unit_price: created_at }
+  scope :filter_by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
+  scope :filter_by_description, (lambda do |description|
+    where('description ILIKE ?', "%#{description}%")
+  end)
+  scope :filter_by_unit_price, ->(unit_price) { where unit_price: unit_price }
+  scope :filter_by_updated_at, ->(updated_at) { where updated_at: updated_at }
+  scope :filter_by_created_at, ->(created_at) { where unit_price: created_at }
 end
