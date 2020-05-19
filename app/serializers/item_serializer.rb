3b787b0e2d2bd662543b595e5b186cf231ce8b6a
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 16:36:25 -0600

    Add updated_at and created_at attributes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
-  attributes :name, :description, :unit_price, :merchant_id
+  attributes :name,
+             :description,
+             :unit_price,
+             :merchant_id,
+             :updated_at,
+             :created_at
 end
