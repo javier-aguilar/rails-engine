d8c6d7c33a752f8a9f2f6418142d3f7b7fc4a345
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 16:38:02 -0600

    Add scopes to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
+  include Filterable
+
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
 
   belongs_to :merchant
+
+  scope :filter_by_name, -> (name) { where('name ILIKE ?', "%#{name}%") }
+  scope :filter_by_description, -> (description) { where('description ILIKE ?', "%#{description}%") }
+  scope :filter_by_unit_price, -> (unit_price) { where unit_price: unit_price }
+  scope :filter_by_updated_at, -> (updated_at) { where updated_at: updated_at }
+  scope :filter_by_created_at, -> (created_at) { where unit_price: created_at }
 end
