24a56de4b159a61536cc61d82da37d057efa33f2
Javier Aguilar <jvr.agx@gmail.com>
2020-05-18 18:13:52 -0600

    Update item factorybot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Games::Zelda.item }
-    description { "MyString" }
-    unit_price { "" }
-    merchant { nil }
+    description { Faker::TvShows::Buffy.quote }
+    unit_price { Faker::Number.decimal(l_digits: 2) }
+    merchant
   end
 end
