c903120df5708d1c04ee63e054dd5b9f3ab120b5
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 16:55:48 -0600

    Add updated_at and created_at attributes

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,7 +1,7 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
-  attributes :name
+  attributes :name, :updated_at, :created_at
   attributes :id, if: proc { |_, params|
     params[:show_id] == true
   }
