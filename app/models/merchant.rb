c78c8e51b50c50e71720dbdd72b22e6d6e6b9f90
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 16:55:32 -0600

    Include module filterable and scopes

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,12 @@
 class Merchant < ApplicationRecord
+  include Filterable
+
   validates :name, presence: true
 
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+
+  scope :filter_by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
+  scope :filter_by_updated_at, ->(updated_at) { where updated_at: updated_at }
+  scope :filter_by_created_at, ->(created_at) { where unit_price: created_at }
 end
