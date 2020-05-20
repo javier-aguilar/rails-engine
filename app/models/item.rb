99d05bbfcfa2922ae0b6ab201d5f66fde928ba31
Javier Aguilar <jvr.agx@gmail.com>
2020-05-20 17:47:40 -0600

    Fix error in scope created_at

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
   end)
   scope :filter_by_unit_price, ->(unit_price) { where unit_price: unit_price }
   scope :filter_by_updated_at, ->(updated_at) { where updated_at: updated_at }
-  scope :filter_by_created_at, ->(created_at) { where unit_price: created_at }
+  scope :filter_by_created_at, ->(created_at) { where created_at: created_at }
 end
