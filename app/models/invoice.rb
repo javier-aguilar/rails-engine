9316b03249d4c146ff7d4e071f9684558d01ef7f
Javier Aguilar <jvr.agx@gmail.com>
2020-05-20 15:12:43 -0600

    Add method in invoice model to get revenue by dates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,13 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items, dependent: :destroy
   has_many :transactions, dependent: :destroy
+
+  def self.revenue_by_dates(dates)
+    joins(:invoice_items, :transactions)
+      .merge(Transaction.successful)
+      .where('invoices.created_at BETWEEN ? AND ?',
+             Date.parse(dates[:start]).beginning_of_day,
+             Date.parse(dates[:end]).end_of_day)
+      .pluck('SUM(invoice_items.quantity * invoice_items.unit_price) AS rev')
+  end
 end
