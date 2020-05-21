114c2fe8db3f7a0e5f9224a45136524b9190d6d6
Javier Aguilar <jvr.agx@gmail.com>
2020-05-20 18:24:44 -0600

    Fix error in scope filter_by_created_at

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,7 +10,7 @@ class Merchant < ApplicationRecord
 
   scope :filter_by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
   scope :filter_by_updated_at, ->(updated_at) { where updated_at: updated_at }
-  scope :filter_by_created_at, ->(created_at) { where unit_price: created_at }
+  scope :filter_by_created_at, ->(created_at) { where created_at: created_at }
 
   def self.most_revenue(limit)
     select('merchants.*,
